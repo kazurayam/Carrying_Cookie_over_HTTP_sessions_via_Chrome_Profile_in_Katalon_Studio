47854525fa95d69e5e386c0bc7c1cc99ef2af668
kazurayam <kazuaki.urayama@gmail.com>
2022-01-04 08:56:45 +0900

    improved GMailLoginTest

--- Include/scripts/groovy/study/GMailLoginTest.groovy
+++ Include/scripts/groovy/study/GMailLoginTest.groovy
@@ -10,9 +10,13 @@ import org.junit.Before
 import org.junit.Test
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.devtools.DevTools
+import org.openqa.selenium.devtools.Event
+
 import org.openqa.selenium.devtools.v96.network.Network
 
 import com.kazurayam.webdriverfactory.chrome.ChromeDriverFactory
+import com.kazurayam.webdriverfactory.UserProfile
+import com.kazurayam.webdriverfactory.chrome.ChromeProfileUtils
 import com.kazurayam.webdriverfactory.chrome.ProfileDirectoryName
 
 import com.kms.katalon.core.webui.driver.DriverFactory
@@ -41,15 +45,21 @@ public class GMailLoginTest {
 
 	@Test
 	public void test_launch_Chrome_with_profileDirectory() {
-		ChromeDriverFactory factory = ChromeDriverFactory.newChromeDriverFactory()
-		driver = factory.newChromeDriver(new ProfileDirectoryName('Default'))
+		ChromeDriverFactory factory = ChromeDriverFactory.newHeadlessChromeDriverFactory()
+		ProfileDirectoryName pdn = new ProfileDirectoryName('Default')
+		driver = factory.newChromeDriver(pdn)
 		assertNotNull(driver)
-		
+		//
+		UserProfile userProfile = ChromeProfileUtils.findUserProfileByProfileDirectoryName(pdn)
+		println("Opening Chrome with")
+		println("    ProfileDirectoryName=" + pdn)
+		println("    UserProfile=" + userProfile)
 		//
 		DevTools devTool = driver.getDevTools()
 		devTool.createSession()
 		devTool.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()))
-		devTool.addListener(Network.requestWillBeSent(), { requestSent ->
+		Event event = Network.requestWillBeSent()
+		devTool.addListener(event, { requestSent ->
 			println("Request URL => " + requestSent.getRequest().getUrl())
 			println("Request Method => " + requestSent.getRequest().getMethod())
 			Gson gson = new GsonBuilder().setPrettyPrinting().create()
